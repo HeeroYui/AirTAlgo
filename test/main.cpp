1863974f8f27848fc1fcd45508b90c7cbe136cbd
Edouard DUPIN <yui.heero@gmail.com>
2017-11-07 13:38:04 +0100

    [DEBUG] build is back

--- test/main.cpp
+++ test/main.cpp
@@ -9,10 +9,7 @@
 #include <etest/etest.hpp>
 
 int main(int _argc, const char** _argv) {
-	// init Google test :
-	::testing::InitGoogleTest(&_argc, const_cast<char **>(_argv));
-	// the only one init for etk:
-	etk::init(_argc, _argv);
+	etest::init(_argc, _argv);
 	for (int32_t iii=0; iii<_argc ; ++iii) {
 		etk::String data = _argv[iii];
 		if (    data == "-h"
